cdf831a7800df09b86c1046636d9971b13cd783e
susumu tomita <oyster880@gmail.com>
2024-01-19 08:37:17 +0900

    update auth

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,12 +1,108 @@
 import express from 'express';
+import passport from 'passport';
+import path from 'path';
+import url from 'url';
+import { Strategy as SamlStrategy } from 'passport-saml';
 
-const app = express();
-const port = 3000;
+const port: string | number = process.env.PORT || 3000;
+const callbackBaseUrl: string = process.env.CALLBACK_BASE_URL || `http://localhost:${port}/`;
+let samlSpKey: string | null = null;
+if (typeof process.env.SAML_SP_KEY !== 'undefined') {
+  samlSpKey = `-----BEGIN PRIVATE KEY-----\n${process.env.SAML_SP_KEY}\n-----END PRIVATE KEY-----`;
+}
 
-app.get('/', (req, res) => {
-  res.send('Hello world update!');
+let samlStrategy = new SamlStrategy(
+  {
+    callbackUrl: url.resolve(callbackBaseUrl, 'saml/login/callback'),
+    entryPoint: process.env.SAML_ENTRY_POINT,
+    issuer: 'passport-saml',
+    cert: process.env.SAML_IDP_CERT || "",
+    decryptionPvk: samlSpKey || "",
+  },
+  function (profile: any, done: any) {
+    let user: any = {};
+    user.saml = profile;
+    user.saml.assertionXml = profile.getAssertionXml();
+    done(null, user);
+  }
+);
+passport.use(samlStrategy);
+
+passport.serializeUser(function (user: any, done: any) {
+  done(null, user);
 });
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+passport.deserializeUser(function (user: any, done: any) {
+  done(null, user);
 });
+
+let app = express();
+
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'ejs');
+
+app.use(require('morgan')('combined'));
+app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production"
+  }
+}));
+app.use(require('helmet')());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/',
+  function (req: express.Request, res: express.Response) {
+    res.render('home', { user: req.user });
+  }
+);
+
+app.get('/logout', function (req: express.Request, res: express.Response) {
+  req.logout(function (err) {
+    if (err) {
+      console.error(err);
+    }
+    res.redirect(process.env.LOGOUT_URL || '/');
+  });
+});
+
+app.get('/saml/login',
+  passport.authenticate('saml')
+);
+
+app.post('/saml/login/callback',
+  passport.authenticate('saml', { failureRedirect: '/' }),
+  function (req: express.Request, res: express.Response) {
+    let walletCreated = createWallet((req.user as any).saml);
+    if (walletCreated) {
+      res.send('Wallet successfully created for the authenticated user.');
+    } else {
+      res.send('Failed to create wallet for the authenticated user.');
+    }
+  }
+);
+
+function createWallet(user: any): boolean {
+  console.log(`Creating wallet for user: ${user}`);
+  return true;
+}
+
+app.get('/saml/metadata',
+  function (req: express.Request, res: express.Response) {
+    let samlSpCert: string | null = null;
+    if (typeof process.env.SAML_SP_CERT !== 'undefined') {
+      samlSpCert = `-----BEGIN CERTIFICATE-----\n${process.env.SAML_SP_CERT}\n-----END CERTIFICATE-----`;
+    }
+    res.type('application/xml');
+    res.send((samlStrategy.generateServiceProviderMetadata(samlSpCert)));
+  }
+);
+
+app.listen(Number(port), '0.0.0.0', () => console.log(`Listening on port ${port}`));
