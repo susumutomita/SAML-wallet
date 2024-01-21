19b584f43d74bcc37bfdde91e74f0db1b540e8c6
susumu tomita <oyster880@gmail.com>
2024-01-21 21:34:32 +0900

    add front

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,18 +1,31 @@
-import { ConnectKitButton } from 'connectkit';
+import { WagmiConfig, createConfig } from "wagmi";
+import { ConnectKitProvider, getDefaultConfig, ConnectKitButton } from "connectkit";
+import React from 'react';
 
-function App() {
+const config = createConfig(
+  getDefaultConfig({
+    // Required API Keys
+    // alchemyId: process.env.REACT_APP_ALCHEMY_ID, // or infuraId
+    // walletConnectProjectId: process.env.REACT_APP_WALLETCONNECT_PROJECT_ID,
+
+    alchemyId: "l7w079rnkih4phmj", // or infuraId
+    walletConnectProjectId: "76c233c79230f517796b63a296786b63",
+
+    // Required
+    appName: "SAML-Wallet",
+  }),
+);
+
+export const App = () => {
+  const handleButtonClick = () => {
+    window.location.href = 'https://saml-wallet-backend.fly.dev/'; // SAMLへのリンクをここに書く
+  };
   return (
-    <div
-      style={{
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        height: '100vh',
-      }}
-    >
-      <ConnectKitButton />
-    </div>
+    <WagmiConfig config={config}>
+      <ConnectKitProvider>
+        <ConnectKitButton />
+        <button onClick={handleButtonClick}>SAMLへのリンク</button>
+      </ConnectKitProvider>
+    </WagmiConfig>
   );
-}
-
-export default App;
+};
---
+++ packages/frontend/src/Home.tsx
@@ -0,0 +1,19 @@
+import { ConnectKitButton } from "connectkit";
+import React from 'react';
+
+function Home() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh"
+      }}
+    >
+      <ConnectKitButton />
+    </div>
+  );
+}
+
+export default Home;
